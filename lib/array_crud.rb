d5846d6da99256f7647de5d747ae662819622451
Stefan Lewyckyj <s80lewyckyj@outlook.com>
2020-08-17 09:48:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [1,2]
+  array = [1,2,3,4 ]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -18,12 +18,10 @@ end
 
 def remove_element_from_end_of_array(array)
   array.pop
-  array
 end
 
 def remove_element_from_start_of_array(array)
   array.shift
-  array
 end
 
 def retrieve_element_from_index(array, index_number)
